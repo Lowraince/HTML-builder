dae0d8ecbd90beab66053ce1de359061540f4629
Lowraince <neson@yahoo.com>
2024-01-21 20:33:30 +0300

    fix: change flag and position writeStream

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,6 +5,8 @@ const pathStyles = path.join(__dirname, 'styles');
 const pathProject = path.join(__dirname, 'project-dist');
 const pathBundle = path.join(pathProject, 'bundle.css');
 
+const writeStream = fs.createWriteStream(pathBundle, { flags: 'w' });
+
 fs.readdir(pathStyles, (err, files) => {
   if (err) {
     console.log(err);
@@ -13,7 +15,6 @@ fs.readdir(pathStyles, (err, files) => {
       if (path.extname(file) === '.css') {
         const filePath = path.join(pathStyles, file);
         const readStream = fs.createReadStream(filePath);
-        const writeStream = fs.createWriteStream(pathBundle, { flags: 'a' });
 
         readStream.on('data', (chunk) => {
           writeStream.write(chunk + '\n', 'utf8');
