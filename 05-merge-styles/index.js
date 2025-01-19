b1fd6cd4cbdfdbb80bfcbb701a856c46650fe42f
Lowraince <neson@yahoo.com>
2025-01-19 17:23:52 +0300

    feat: 05 complete task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToStyles = path.join(__dirname, 'styles');
+const pathToProject = path.join(__dirname, 'project-dist');
+
+const pathToOutputFile = path.join(pathToProject, 'bundle.css');
+
+const streamWS = fs.createWriteStream(pathToOutputFile);
+
+fs.readdir(pathToStyles, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+    const fileStylePath = path.join(pathToStyles, file.name);
+    const fileExt = path.extname(file.name);
+
+    const streamRS = fs.createReadStream(fileStylePath, 'utf-8');
+
+    if (file.isFile() && fileExt === '.css') {
+      streamRS.on('data', (chunk) => {
+        streamWS.write(chunk);
+      });
+
+      streamRS.on('end', () => {
+        streamWS.write('\n');
+      });
+
+      streamRS.on('error', (err) => console.error('Error:', err.message));
+    }
+  });
+});
+
+streamWS.on('error', (err) => console.error('Error:', err.message));
